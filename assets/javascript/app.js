7d7c2b13f2e34c259fdf5f60fb9ca4f9beb641b5
unknown <mmreyes13@gmail.com>
2017-08-26 12:49:22 -0400

    query url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,3 +2,14 @@
 var $searchTerm = $("#search");
 var $numberOfResults = $("#placeHolder");//this variable will feed the offset parameter. Also used to set i in display loop.
 var $startYr = $();
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + "q=" + searchTerm + "&page=" + numberOfResults + "&begin_date=" + startYr + "&end_date=" + endYr + "&sort=newest&api-key=a1c5f475049643e28957a815b74318b4";
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
