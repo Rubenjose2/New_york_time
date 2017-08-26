0563056ae8120a57d9695039d7a84e6cb760b389
Bayrex Rodriguez <bayrexr@gmail.com>
2017-08-26 12:24:54 -0400

    added first set of variables to app.js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+//declare variables
+var $searchTerm = $("#search");
+var $numberOfResults = $("#placeHolder");//this variable will feed the offset parameter. Also used to set i in display loop.
+var $startYr = $();
